7aa90ac16772bb3a43d22211aac00de98de35e83
Antonio Echenique <bimodalitil08@gmail.com>
2017-06-22 11:42:35 -0400

    1.0.0

---
+++ WebApiResful/Controllers/ClientController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiResful.Models;
+
+namespace WebApiResful.Controllers
+{
+    public class ClientController : ApiController
+    {
+        
+        public IEnumerable<Client> GetAllClient()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client { id = 1, name = "Antonio", lastname = "Echenique" });
+            clients.Add(new Client { id = 2, name = "Erika", lastname = "Echenique" });
+            clients.Add(new Client { id = 3, name = "Edecio", lastname = "Echenique" });
+            return clients;
+        }
+
+        public IHttpActionResult GetClient(int id)
+        {
+            List<Client> clients = new List<Client>(GetAllClient());
+            var client = clients.FirstOrDefault((c) => c.id == id);
+            if(client != null)
+            {
+                return Ok(client);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        
+
+
+    }
+}
---
+++ WebApiResful/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiResful.Models
+{
+    public class Client
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+    }
+
+    
+}
